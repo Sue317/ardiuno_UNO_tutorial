f88880b50c37671e5c8dc4ce1a0e70185d45c66d
Sue317 <212111@nnjh.tn.edu.tw>
2023-10-21 13:22:27 +0800

    Update 04_LED_blink_randomly.ino

--- example/04_LED_blink_randomly/04_LED_blink_randomly.ino
+++ example/04_LED_blink_randomly/04_LED_blink_randomly.ino
@@ -1,4 +1,4 @@
-byte LED_pin = 0; // 設定想要控制的 LED 腳位編號
+byte LED = 0; // 設定想要控制的 LED 腳位編號
 byte t = 0;       // 設定要持續多少秒
 
 void setup() {
@@ -9,10 +9,10 @@ void setup() {
 }
 
 void loop() {
-  LED = random(2,6);      // 隨機決定欲控制 LED 腳位編號
+  LED = random(2,4);      // 隨機決定欲控制 LED 腳位編號
   t = random(1,4);        // 隨機決定秒數
   digitalWrite(LED,HIGH); // 讓該 LED 亮起
-  delay(t * 1000);        // 持續亮 t 秒
+  delay(t * 200);        // 持續亮 t 秒
   digitalWrite(LED,LOW);  // 熄掉 LED
-  delay(t * 1000);        // 持續熄 t 秒
+  delay(t * 200);        // 持續熄 t 秒
 }
