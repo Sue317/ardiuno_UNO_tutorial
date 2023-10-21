d30ad2b0125c39622b8c36c5d6c8a4579bff6fb6
Sue317 <212111@nnjh.tn.edu.tw>
2023-10-21 16:10:28 +0800

    Update 11_buzzer_song_BPM.ino

--- example/11_buzzer_song_BPM/11_buzzer_song_BPM.ino
+++ example/11_buzzer_song_BPM/11_buzzer_song_BPM.ino
@@ -2,43 +2,39 @@
 
 int buzzerPin = 9;        // 定義蜂鳴器引腳
 int size;
-float BPM = 120.0;                // Beats Per minute
+float BPM = 200.0;                // Beats Per minute
 int quarter = (60/BPM)*1000;      // 四分音符
 int half = quarter * 2;           // 半分音符
 int eigth = quarter / 2;          // 八分音符
 
-void set_beats(float BPM, int beats[]) {
-  float BPS;
-  BPS = BPM / 60.0;               // 換成 Beats Per second
-  beats[1] = round(BPS * 1000);
-  beats[0] = round(beats[1]*2);
-  beats[2] = round(beats[1]/2);
-}
 
 // 簡譜對照表
 // 1: C4, 2: D4, 3: E4, 4: F4, 5: G4, 6: A4
 // 5. : G3
 
 // 用陣列儲存兩隻老虎的旋律
-int melody[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
-                NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
-                NOTE_E4, NOTE_F4, NOTE_G4,
-                NOTE_E4, NOTE_F4, NOTE_G4,
-                NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
-                NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
-                NOTE_D4, NOTE_G3, NOTE_C4,
-                NOTE_D4, NOTE_G3, NOTE_C4
+int melody[] = {NOTE_A4, NOTE_B4,
+ NOTE_C4, NOTE_A4,
+                NOTE_E4, NOTE_C4, NOTE_B4, NOTE_E4,
+                NOTE_C4, NOTE_A4, NOTE_F4, NOTE_C4,
+                NOTE_A4, NOTE_G4, NOTE_G4,
+                NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C2, NOTE_A4,
+                NOTE_B4, NOTE_C4, NOTE_D4, NOTE_D4, NOTE_C4, NOTE_B4,
+                NOTE_B4, NOTE_A4, NOTE_C4,
+                NOTE_A4, NOTE_E4, NOTE_C4
                 };
 
 // 用陣列儲存兩隻老虎的節奏
-int noteDurations[] = { quarter, quarter, quarter, quarter,
-                        quarter, quarter, quarter, quarter,
-                        quarter, quarter, half,
-                        quarter, quarter, half,
-                        eigth, eigth, eigth, eigth, quarter, quarter,
-                        eigth, eigth, eigth, eigth, quarter, quarter,
-                        quarter, quarter, half,
-                        quarter, quarter, half
+int noteDurations[] = { eigth, eigth,
+                        eigth, eigth, 1.5*quarter, quarter,
+                        eigth, eigth, eigth, 
+                        eigth, 1.5*quarter, quarter, quarter,
+                        quarter, eigth,
+                        eigth, eigth, eigth,
+                        eigth, eigth, eigth, eigth, quarter, eigth,
+                        eigth, eigth, eigth, quarter, eigth, eigth,
+                        eigth, eigth, 1.5*quarter,
+                        quarter, eigth, eigth
                       };
 
 void setup() {
