527e2c64703ac56abc027180b5034459b3271387
Sue317 <212111@nnjh.tn.edu.tw>
2023-10-22 10:29:41 +0800

    Update 13_photoResistor_LED_using_if.ino

--- example/13_photoResistor_LED_using_if/13_photoResistor_LED_using_if.ino
+++ example/13_photoResistor_LED_using_if/13_photoResistor_LED_using_if.ino
@@ -1,10 +1,14 @@
 int photoResistor = A0; // 設定光敏電阻類比訊號的腳位
 int LEDpin = 2;         // 設定 LED 的腳位 數位(digital)腳位
+int LEDpin2 = 3;
 int value;              // 儲存光敏電阻的類比訊號值
 
 void setup() {
   pinMode(photoResistor, INPUT);
   pinMode(LEDpin, OUTPUT);
+  digitalWrite(LEDpin, LOW);
+  pinMode(LEDpin2, OUTPUT);
+  digitalWrite(LEDpin2, LOW);
   Serial.begin(9600);
 }
 
@@ -13,15 +17,19 @@ void loop() {
   Serial.print("Analog signal: ");
   Serial.println(value);
   
-  if(value > 10 & value < 20) {     // 判斷 value 是否介於 10(不包含) 到 20 之間
+  if(value > 100 & value < 200) {     // 判斷 value 是否介於 10(不包含) 到 20 之間
     Serial.println("It is dim.");
-  } else if(value <= 10) {          // 判斷 value 是否小於等於 10
+    digitalWrite(LEDpin, HIGH); 
+    digitalWrite(LEDpin2, LOW); 
+  } else if(value > 200) {          // 判斷 value 是否小於等於 10
     Serial.println("It is dark.");
-    digitalWrite(LEDpin, HIGH);     // 數位訊號給高電壓 讓 LED 亮
-  } else {                          // 其他狀況
+    digitalWrite(LEDpin, LOW);     // 數位訊號給高電壓 讓 LED 亮
+    digitalWrite(LEDpin2, HIGH);
+  } else {                       // 其他狀況
     Serial.println("It is bright.");
     digitalWrite(LEDpin, LOW);      // 數位訊號給低電壓 不讓 LED 做事
+    digitalWrite(LEDpin2, LOW);
   }
-  delay(1000);
+  delay(500);
 }
 
