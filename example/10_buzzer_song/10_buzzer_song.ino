37894c103f9e9bd811e662db54d5953ff0541f40
Sue317 <212111@nnjh.tn.edu.tw>
2023-10-28 16:09:32 +0800

    update

--- example/10_buzzer_song/10_buzzer_song.ino
+++ example/10_buzzer_song/10_buzzer_song.ino
@@ -1,10 +1,34 @@
+float BPM = 200.0;                // Beats Per minute
+int quarter = (60/BPM)*1000;      // 四分音符
+int half = quarter * 2;           // 半分音符
+int eigth = quarter / 2;          // 八分音符
+int size;
+
+int beats[] = {
+  quarter, quarter, eigth, eigth, quarter,
+quarter, quarter, eigth, eigth, quarter,
+quarter, eigth, eigth, quarter, eigth, eigth, 
+eigth, eigth, eigth, eigth, eigth, eigth, eigth, eigth,
+
+};
+
 void setup() {
   pinMode(2,OUTPUT);  // 設定腳位 2 為輸出
+  size = sizeof(beats) / sizeof(beats[0]);  // 計算儲存整首歌陣列的大小
+  digitalWrite(2,LOW);
+  delay(1000);
 }
 
 void loop() {
-  digitalWrite(2,HIGH); // 輸出高電位，會叫
-  delay(1000);
-  digitalWrite(2,LOW);  // 輸出低電位，不會叫
-  delay(1000);
+
+  for (int i = 0; i < size; i++) {
+    digitalWrite(2,HIGH); // 輸出高電位，會叫
+    delay(beats[i]);
+    digitalWrite(2,LOW);
+    delay(beats[i]);
+  }
+  // digitalWrite(2,HIGH); // 輸出高電位，會叫
+  // delay(1000);
+  // digitalWrite(2,LOW);  // 輸出低電位，不會叫
+  // delay(1000);
 }
--- example/11_buzzer_song_BPM/11_buzzer_song_BPM.ino
+++ example/11_buzzer_song_BPM/11_buzzer_song_BPM.ino
@@ -2,46 +2,38 @@
 
 int buzzerPin = 9;        // 定義蜂鳴器引腳
 int size;
-float BPM = 200.0;                // Beats Per minute
+float BPM = 124.0;                // Beats Per minute
 int quarter = (60/BPM)*1000;      // 四分音符
 int half = quarter * 2;           // 半分音符
 int eigth = quarter / 2;          // 八分音符
+int sixteen = quarter / 4;
 
 
 // 簡譜對照表
 // 1: C4, 2: D4, 3: E4, 4: F4, 5: G4, 6: A4
 // 5. : G3
 
-// 用陣列儲存兩隻老虎的旋律
-int melody[] = {NOTE_A4, NOTE_B4,
-                NOTE_C4, NOTE_A4, NOTE_E4, NOTE_C4, 
-                NOTE_B4, NOTE_E4, NOTE_C4, 
-                NOTE_A4, NOTE_F4, NOTE_C4, NOTE_A4, 
-                NOTE_G4, NOTE_G4,
-                NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, 
-                NOTE_C2, NOTE_A4, NOTE_B4, NOTE_C4, 
-                NOTE_D4, NOTE_D4, NOTE_C4, NOTE_B4, NOTE_B4, 
-                NOTE_A4, NOTE_C4, NOTE_A4, 
-                NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4
-                };
 
-// 用陣列儲存兩隻老虎的節奏
-int noteDurations[] = { eigth, eigth,
-                        eigth, eigth, 1.5*quarter, quarter,
-                        eigth, eigth, eigth, 
-                        eigth, 1.5*quarter, quarter, quarter,
-                        quarter, eigth,
-                        eigth, eigth, eigth, eigth, 
-                        eigth, eigth, eigth, quarter,
-                        quarter, eigth, eigth, eigth, eigth,  
-                        quarter, eigth, eigth, 
-                        eigth, eigth, 1.5*quarter, quarter,
-                      };
+int melody[] = {
+  NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4,
+  NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_B4, NOTE_B4, NOTE_B4, 0,
+  NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4,  
+  NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_A4, NOTE_A4, NOTE_A5, NOTE_A5, NOTE_G6, NOTE_G6, NOTE_A7, NOTE_D7, 
+};
+
+int noteDurations[] = {
+  eigth, eigth, eigth, eigth, eigth, eigth, eigth, eigth,
+  eigth, eigth, eigth, eigth, eigth, eigth, sixteen, sixteen, sixteen, sixteen, 
+  eigth, eigth, eigth, eigth, eigth, eigth, eigth, eigth,
+  eigth, eigth, eigth, eigth, sixteen, sixteen, sixteen, sixteen, sixteen, sixteen, sixteen, sixteen,
+  
+};
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);                 // 設置蜂鳴器腳位為輸出
   Serial.begin(9600);
   size = sizeof(melody) / sizeof(melody[0]);  // 計算儲存整首歌陣列的大小
+  delay(1000);
 }
 
 void loop() {
@@ -53,5 +45,5 @@ void loop() {
     delay(pauseBetweenNotes);
     noTone(buzzerPin);
   }
-  delay(1000);  // 停頓一秒
+  delay(10);  // 停頓一秒
 }
