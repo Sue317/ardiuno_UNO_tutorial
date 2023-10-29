84e3fcace7b8c381afedc64b88d34fb65774205e
Sue317 <212111@nnjh.tn.edu.tw>
2023-10-29 10:43:17 +0800

    Update mySong.ino

--- mySong/mySong.ino
+++ mySong/mySong.ino
@@ -1,11 +1,12 @@
-#include "notes.h"        // 引入函式庫
+#include "notes.h"       // 引入函式庫
 
 int buzzerPin = 9;        // 定義蜂鳴器引腳
 int size;
-float BPM = 200.0;                // Beats Per minute
+float BPM = 124.0;                // Beats Per minute
 int quarter = (60/BPM)*1000;      // 四分音符
 int half = quarter * 2;           // 半分音符
 int eigth = quarter / 2;          // 八分音符
+int sixteen = quarter / 4;
 
 
 // 簡譜對照表
@@ -13,18 +14,23 @@ int eigth = quarter / 2;          // 八分音符
 // 5. : G3
 
 
-int melody[] = {
-  NOTE_G4 , NOTE_G4 , NOTE_C4 , NOTE_C4 , NOTE_A4 ,
-  NOTE_G4 , NOTE_G4 , NOTE_C4 , NOTE_C4 , NOTE_A4 ,
-  NOTE_C4, NOTE_E4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_E4,
-  NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4,
+int melody[] = { 
+  NOTE_C4, NOTE_D4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_C4, 
+  NOTE_C4, NOTE_D4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4,  NOTE_E4, NOTE_C4, NOTE_B4, NOTE_B4, NOTE_B4, 0,
+  NOTE_C4, NOTE_D4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_C4,  
+  NOTE_C4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_A4, NOTE_A4, NOTE_A5, NOTE_A5, NOTE_G6, NOTE_G6, NOTE_A7, NOTE_D7, 
+  //NOTE_E6, 
+  //NOTE_D3, NOTE_C3, 
+
 };
 
 int noteDurations[] = {
-  quarter, quarter, eigth, eigth, quarter,
-quarter, quarter, eigth, eigth, quarter,
-quarter, eigth, eigth, quarter, eigth, eigth, 
-eigth, eigth, eigth, eigth, eigth, eigth, eigth, eigth,
+  eigth, eigth, eigth, eigth, eigth, eigth, eigth, eigth,
+  eigth, eigth, sixteen, sixteen, sixteen, sixteen, eigth, eigth, sixteen, sixteen, sixteen, sixteen, 
+  eigth, eigth, eigth, eigth, eigth, eigth, eigth, eigth,
+  eigth, eigth, eigth, eigth, sixteen, sixteen, sixteen, sixteen, sixteen, sixteen, sixteen, sixteen,
+  //eigth, 
+  //sixteen, sixteen, 
 
 };
 
@@ -32,6 +38,7 @@ void setup() {
   pinMode(buzzerPin, OUTPUT);                 // 設置蜂鳴器腳位為輸出
   Serial.begin(9600);
   size = sizeof(melody) / sizeof(melody[0]);  // 計算儲存整首歌陣列的大小
+  delay(1000);
 }
 
 void loop() {
@@ -45,3 +52,4 @@ void loop() {
   }
   delay(10);  // 停頓一秒
 }
+
